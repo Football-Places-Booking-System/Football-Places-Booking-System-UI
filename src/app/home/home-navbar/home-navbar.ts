ffa5654c03b87299cd9bb0606426a4ab8d88a483
Mahd Mohamed <engmahd00@gmail.com>
2025-07-23 18:15:12 +0300

    home page finished

--- src/app/home/home-navbar/home-navbar.ts
+++ src/app/home/home-navbar/home-navbar.ts
@@ -4,11 +4,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-section',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, RouterModule],
   templateUrl: './home-navbar.html',
   styleUrl: './home-navbar.css'
 })
@@ -19,5 +20,12 @@ export class Navbar {
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.scrollY > 60;
+  }
+
+  scrollToSection(sectionId: string) {
+    const el = document.getElementById(sectionId);
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
   }
 }
